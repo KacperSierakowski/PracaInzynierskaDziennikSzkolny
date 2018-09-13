65e3472e0e641aa680f1a3d85a3f187c6a33dafc
Student218608 <218608@student.pwr.edu.pl>
2018-09-13 16:36:07 +0200

    First

---
+++ DziennikSzkolny13/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DziennikSzkolny13.MvcApplication" Language="C#" %>
---
+++ DziennikSzkolny13/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DziennikSzkolny13.Startup))]
+namespace DziennikSzkolny13
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
